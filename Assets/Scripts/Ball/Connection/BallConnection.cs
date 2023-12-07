200bac0aa7f5588a461f79389cbd8c2a7d9c6376
Sora(HinataSato) <hinata.act.2@gmail.com>
2023-12-08 03:49:16 +0900

    2つ以上接触したときに接触したものすべってが変換されるのを修正

--- Assets/Scripts/Ball/Connection/BallConnection.cs
+++ Assets/Scripts/Ball/Connection/BallConnection.cs
@@ -3,6 +3,8 @@ using Constants;
 using Ball_Next;
 using Lean.Pool;
 using UniRx;
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace BAll_Connection
 {
@@ -11,6 +13,8 @@ namespace BAll_Connection
         [SerializeField] private CBallType _ballType;
         public CBallType BallType => _ballType;
         [HideInInspector] public bool IsDrop = false;
+        private bool _isConnect = false;
+        public bool IsConnect => _isConnect;
         private IBallController _ballController;
 
         private void Awake()
@@ -24,6 +28,12 @@ namespace BAll_Connection
                 .Where(x => IsDrop)
                 .Subscribe(x => SetGravity(x))
                 .AddTo(gameObject);
+        }
+
+        private void OnDisable()
+        {
+            _isConnect = false;
+            IsDrop = false;
         }
 
         /// <summary>
@@ -50,6 +60,8 @@ namespace BAll_Connection
             float thisDistance = (originPos - transform.localPosition).sqrMagnitude;
             float targetDistance = (originPos - target.transform.localPosition).sqrMagnitude;
 
+            Debug.Log(IsConnect);
+            Debug.Log(gameObject.name + "++" + target.gameObject.name + "++" + thisDistance + "++ " + targetDistance);
             if (thisDistance < targetDistance)
             {
                 return true;
@@ -82,14 +94,18 @@ namespace BAll_Connection
             if (other.collider.CompareTag(TagName.Ball) &&
                 other.collider.GetComponent<BallConnection>().BallType == _ballType)
             {
-                if (IsSpwanPosition(other.gameObject.transform))
+                if (!_isConnect && !other.collider.GetComponent<BallConnection>().IsConnect)
                 {
-                    GameStore.Instance.GameData.AddScoreType(_ballType);
-                    ConnectBall();
+                    _isConnect = true;
+                    if (IsSpwanPosition(other.gameObject.transform))
+                    {
+                        GameStore.Instance.GameData.AddScoreType(_ballType);
+                        ConnectBall();
+                    }
+                    GetComponent<Rigidbody2D>().gravityScale = 0;
+                    IsDrop = false;
+                    LeanPool.Despawn(gameObject);
                 }
-                GetComponent<Rigidbody2D>().gravityScale = 0;
-                IsDrop = false;
-                LeanPool.Despawn(gameObject);
             }
         }
     }
